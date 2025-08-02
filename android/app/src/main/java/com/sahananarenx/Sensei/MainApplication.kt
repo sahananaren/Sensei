ec911272a56b01fd1c81ae97c5e0c1ec8601d1ae
sahananarenx@gmail.com <sahananarenx@gmail.com>
2025-08-02 12:01:47 +0530

    Fix Kotlin compilation errors: remove BuildConfig references and fix package names

--- android/app/src/main/java/com/sahananarenx/Sensei/MainApplication.kt
+++ android/app/src/main/java/com/sahananarenx/Sensei/MainApplication.kt
@@ -32,8 +32,8 @@ class MainApplication : Application(), ReactApplication {
 
           override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
-          override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-          override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+          override val isNewArchEnabled: Boolean = false
+          override val isHermesEnabled: Boolean = true
       }
   )
 
